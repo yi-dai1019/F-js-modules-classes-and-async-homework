42ae53e1d1e6d6ffc7f1c468e0d068d3770f279e
yi-dai <yi.dai@thoughtworks.com>
2020-11-10 21:44:53 +0800

    practice3 with async

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,11 +1,10 @@
-export const getPoetry = () => {
-  return fetch("https://v1.jinrishici.com/all.json", {
-    method: "GET",
-  })
-    .then((response) => response.json())
-    // eslint-disable-next-line prettier/prettier
-    .then (({ content, origin, author } ) => [ origin, author, content ])
-    .catch((error) => {
-      console.log(error);
-    });
+export const getPoetry = async () => {
+    try{
+        const response = await fetch("https://v1.jinrishici.com/all.json");
+        const { origin, author, content } = await response.json();
+        // eslint-disable-next-line prettier/prettier
+        return [ origin, author, content ];
+    } catch(error) {
+        console.log(error);
+    }
 };
