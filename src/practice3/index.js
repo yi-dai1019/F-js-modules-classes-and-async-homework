591cd54e49c45bad934c10dc1b145ec94d26b747
yi-dai <yi.dai@thoughtworks.com>
2020-11-10 21:38:04 +0800

    practice3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,11 @@
-export const getPoetry
+export const getPoetry = () => {
+  return fetch("https://v1.jinrishici.com/all.json", {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    // eslint-disable-next-line prettier/prettier
+    .then (({ content, origin, author } ) => [ origin, author, content ])
+    .catch((error) => {
+      console.log(error);
+    });
+};
